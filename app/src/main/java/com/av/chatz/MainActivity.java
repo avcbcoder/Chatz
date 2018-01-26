6b0be350b503c0ecb0c28e5c0121ae185febf38b
Ankit Verma <ankitvermamat@gmail.com>
2018-01-27 02:02:53 +0530

    Toolbar Added

--- app/src/main/java/com/av/chatz/MainActivity.java
+++ app/src/main/java/com/av/chatz/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,12 +12,18 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //firebase authentication Instance
         mAuth = FirebaseAuth.getInstance();
+        //Toolbar
+        toolbar = findViewById(R.id.ma_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("CHATZ");
     }
 
     @Override
